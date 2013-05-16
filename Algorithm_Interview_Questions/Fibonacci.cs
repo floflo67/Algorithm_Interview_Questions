733ef8062704f0e6ab417362aba6bd82e1a920e6
Reiss Florian <85802@supinfo.com>
2013-05-16 15:06:40 -0700

    Project creation

---
+++ Algorithm_Interview_Questions/Fibonacci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Interview_Questions
+{
+    class Fibonacci
+    {
+        /*Fibonacci is the suite:
+         * 1; 1; 2; 3; 5; etc...
+         * you have u(n) = u(n-1) + u(n-2)
+         * with u(0) = 0 and u(1) = 1
+         */
+
+        public int Recurrence(int value)
+        {
+
+        }
+    }
+}
