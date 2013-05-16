c7abbd43e8441b08e56a35b9302010d030d65dbc
Reiss Florian <85802@supinfo.com>
2013-05-16 15:25:24 -0700

    Reverse Fibonacci
    I reverse Fibonacci by using the Recurrence method.

--- Algorithm_Interview_Questions/Fibonacci.cs
+++ Algorithm_Interview_Questions/Fibonacci.cs
@@ -22,11 +22,38 @@ namespace Algorithm_Interview_Questions
         /// <returns>The fibonacci number at the index "value"</returns>
         public int Recurrence(int value)
         {
-            if(value == 0)
+            if(value == 0) // u(0) = 0
                 return 0;
-            if(value == 1)
+            if(value == 1) // u(1) = 1
                 return 1;
             return (Recurrence(value - 1) + Recurrence(value - 2));
+        }
+
+        /// <summary>
+        /// Will tell you the index of the number
+        /// Works only with "real" fibonacci number
+        /// </summary>
+        /// <param name="number">The number to analyze</param>
+        /// <returns>The index in the fibonacci suite where you can find "number"</returns>
+        public int ReverseUsingRecurrence(int number)
+        {
+            if(number == 0)
+                return 0;
+            if(number == 1)
+                return 1;
+
+            int numberRec = 1;
+            int indexRec = 2;
+            while(numberRec < number)
+            {
+                numberRec = Recurrence(indexRec);
+
+                if(numberRec == number)
+                    return indexRec;
+                else
+                    indexRec++;
+            }
+            return 0;
         }
     }
 }
