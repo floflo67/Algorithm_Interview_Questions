90e8b50b7858eca9b52d21d177c5e7ff6dfd41e9
Reiss Florian <85802@supinfo.com>
2013-05-16 16:09:52 -0700

    First : Palindrome
    Algorithm on Palindrome using recurrence or not

--- Algorithm_Interview_Questions/UsingStrings.cs
+++ Algorithm_Interview_Questions/UsingStrings.cs
@@ -12,19 +12,52 @@ namespace Algorithm_Interview_Questions
          * First: if palindrome or not
          */
 
-
         // a palindrome is a word/sentence that you can read both ways
         // example : lol, kayak, level, redder, madam, etc...
-        public bool Palidrome(string word)
+
+        /// <summary>
+        /// Will tell you if "word" is a palindrome
+        /// We use recurrence
+        /// </summary>
+        /// <param name="word">The word to check</param>
+        /// <returns>You will get TRUE if "word" is a palindrome</returns>
+        public bool PalidromeUsingRecurrence(string word)
         {
             int length = word.Length;
 
             if(word.ElementAt(0) == word.ElementAt(length-1) && length <= 3)
                 return true;
             else if(word.ElementAt(0) == word.ElementAt(length-1))
-                return Palidrome(word.Substring(1,length-2));
+                return PalidromeUsingRecurrence(word.Substring(1,length-2));
 
             return false;
+        }
+
+        /// <summary>
+        /// Will tell you if "word" is a palindrome
+        /// We don't use recurrence
+        /// We create a table to contain the word
+        /// </summary>
+        /// <param name="word">The word to check</param>
+        /// <returns>You will get TRUE if "word" is a palindrome</returns>
+        public bool PalindromeWithoutRecurrence(string word)
+        {
+            int length = word.Length;
+            char[] tableChar = new char[length];
+
+            int i = 0;
+            foreach(char c in word)
+            {
+                tableChar[i] = c;
+                i++;
+            }
+
+            for(int j = 0 ; j < length / 2 ; j++)
+            {
+                if(tableChar[j] != tableChar[length - j - 1])
+                    return false;
+            }
+            return true;
         }
     }
 }
