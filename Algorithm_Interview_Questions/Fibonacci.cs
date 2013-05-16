d540a6704bba811e5d507a44dcfa995a1ccdbb13
Reiss Florian <85802@supinfo.com>
2013-05-16 15:12:26 -0700

    Fibonacci with Recurrence

--- Algorithm_Interview_Questions/Fibonacci.cs
+++ Algorithm_Interview_Questions/Fibonacci.cs
@@ -9,14 +9,24 @@ namespace Algorithm_Interview_Questions
     class Fibonacci
     {
         /*Fibonacci is the suite:
-         * 1; 1; 2; 3; 5; etc...
+         * 0; 1; 1; 2; 3; 5; etc...
          * you have u(n) = u(n-1) + u(n-2)
          * with u(0) = 0 and u(1) = 1
          */
 
+        /// <summary>
+        /// Construction by recurrence
+        /// The easiest and fastest way to do it
+        /// </summary>
+        /// <param name="value">The index</param>
+        /// <returns>The fibonacci number at the index "value"</returns>
         public int Recurrence(int value)
         {
-
+            if(value == 0)
+                return 0;
+            if(value == 1)
+                return 1;
+            return (Recurrence(value - 1) + Recurrence(value - 2));
         }
     }
 }
