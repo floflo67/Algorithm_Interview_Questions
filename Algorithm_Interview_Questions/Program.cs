d4bc10a0962c5766a3e1ef72d5555b079e3bbaca
Reiss Florian <85802@supinfo.com>
2013-05-16 16:49:07 -0700

    Test in Program.cs

--- Algorithm_Interview_Questions/Program.cs
+++ Algorithm_Interview_Questions/Program.cs
@@ -10,6 +10,27 @@ namespace Algorithm_Interview_Questions
     {
         static void Main(string[] args)
         {
+            #region Fibonacci
+            //Fibonacci fb = new Fibonacci();
+
+            //Console.WriteLine(fb.Recurrence(4)); // should give us 3
+            //Console.WriteLine(fb.Recurrence(7)); // should give us 13
+
+            //Console.WriteLine(fb.ReverseUsingRecurrenceMethod(13)); // should give us 7
+            //Console.WriteLine(fb.ReverseUsingRecurrenceMethod(3)); // should give us 4
+            #endregion
+
+            #region Using Strings
+            UsingStrings us = new UsingStrings();
+
+            //Console.WriteLine(us.PalidromeUsingRecurrence("level")); // should give us TRUE
+            //Console.WriteLine(us.PalidromeUsingRecurrence("rever")); // should give us TRUE
+            //Console.WriteLine(us.PalidromeUsingRecurrence("true")); // should give us FALSE
+
+            Console.WriteLine(us.PalindromeWithoutRecurrence("level")); // should give us TRUE
+            Console.WriteLine(us.PalindromeWithoutRecurrence("rever")); // should give us TRUE
+            Console.WriteLine(us.PalindromeWithoutRecurrence("true")); // should give us FALSE
+            #endregion
 
             Console.Read();
         }
