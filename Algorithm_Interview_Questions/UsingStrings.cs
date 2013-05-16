f1de3b0f420dd3901f59efbc865b7618a5a89d3e
Reiss Florian <85802@supinfo.com>
2013-05-16 15:55:30 -0700

    Use of strings
    First : Palindrome

---
+++ Algorithm_Interview_Questions/UsingStrings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Interview_Questions
+{
+    class UsingStrings
+    {
+        /* Let's do a few funny things using strings
+         * First: if palindrome or not
+         */
+
+
+        // a palindrome is a word/sentence that you can read both ways
+        // example : lol, kayak, level, redder, madam, etc...
+        public bool Palidrome(string word)
+        {
+            int length = word.Length;
+
+            if(word.ElementAt(0) == word.ElementAt(length-1) && length <= 3)
+                return true;
+            else if(word.ElementAt(0) == word.ElementAt(length-1))
+                return Palidrome(word.Substring(1,length-2));
+
+            return false;
+        }
+    }
+}
